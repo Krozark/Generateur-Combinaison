2cbdfd2f966a9d18c488c3262a1d2589bf552279
Krozark <maxime.barbier1991@gmail.com>
2012-12-20 14:00:08 +0100

    update main

--- classe/main.cpp
+++ classe/main.cpp
@@ -1,10 +1,17 @@
 #include "Combinaison.hpp"
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc,char * argv[])
 {
-    Combinaison::returnType* res = Combinaison::genAllFor(6);
-    Combinaison::__print__(*res);
-    delete res;
+    if (argc > 1)
+    {
+        Combinaison::returnType* res = Combinaison::genAllFor(atoi(argv[1]));
+        Combinaison::__print__(*res);
+        delete res;
+    }
+    else
+        printf("manque le nombre en param\n");
 
     return 0;
 }
